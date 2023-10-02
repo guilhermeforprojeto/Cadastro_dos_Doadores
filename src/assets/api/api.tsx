6b6a57b4a6e6290ca8f6227602ce2dfd88122d0b
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-10-01 21:14:13 -0300

    update: api

--- src/assets/api/api.tsx
+++ src/assets/api/api.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 // Define as configurações padrões quando cria a instância
 export const API = axios.create({
-  baseURL: 'http://system-bee.ddns.net:8080/'
+  baseURL: 'http://systembeebygui.ddns.net:8022/'
   // baseURL: 'http://192.168.15.2:8080'
-  // baseURL: 'http://system-bee.ddns.net:8022'
+  // baseURL: 'http://systembeebygui.ddns.net:8022'
 });
 
 // // Altera as configurações padrões após a instância ser criada
