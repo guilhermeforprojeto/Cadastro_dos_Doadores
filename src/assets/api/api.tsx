8f5657b514c8c79541adedef4e4c2d4d375bfbc1
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-10-03 23:24:12 -0300

    fix link api backend

--- src/assets/api/api.tsx
+++ src/assets/api/api.tsx
@@ -1,22 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://192.168.15.2:8080'
+  // baseURL: 'http://192.168.15.2:8080'
+  baseURL: 'http://systembeebygui.ddns.net:8022/'
+
 });
-
-API.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response && error.response.status === 503) {
-      // O servidor não está respondendo
-      return axios({
-        method: error.request.method,
-        url: 'http://systembeebygui.ddns.net:8080' + error.request.url,
-        data: error.request.data
-      });
-    }
-
-    // Retorna o erro original
-    return Promise.reject(error);
-  }
-);
